a29a04dcf2670dd99001fd8a471aac2a04a6bbea
yinm <yinm.cminor9@gmail.com>
2017-07-25 07:25:30 +0900

    パッケージと構成

---
+++ chapter02/32-33/animals.go
@@ -0,0 +1,13 @@
+package animals
+
+func ElephantFeed() string {
+	return "Grass"
+}
+
+func MonkeyFeed() string {
+	return "Banana"
+}
+
+func RabbitFeed() string {
+	return "Carrot"
+}
---
+++ chapter02/32-33/zoo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"./animals"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
