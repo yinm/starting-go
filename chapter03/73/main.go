af01df0f0aef2ee1f548bf562058d2580da66472
yinm <yinm.cminor9@gmail.com>
2017-07-29 15:34:26 +0900

    float64型の丸め確認

---
+++ chapter03/73/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("value = %v\n", 1.0000000000000000)
+	fmt.Printf("value = %v\n", 1.0000000000000001)
+	fmt.Printf("value = %v\n", 1.0000000000000002)
+	fmt.Printf("value = %v\n", 1.0000000000000003)
+	fmt.Printf("value = %v\n", 1.0000000000000004)
+	fmt.Printf("value = %v\n", 1.0000000000000005)
+	fmt.Printf("value = %v\n", 1.0000000000000006)
+	fmt.Printf("value = %v\n", 1.0000000000000007)
+	fmt.Printf("value = %v\n", 1.0000000000000008)
+	fmt.Printf("value = %v\n", 1.0000000000000009)
+}
