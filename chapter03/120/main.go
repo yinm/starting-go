122a77a72c59effb5ce430baef6ac60ad42b46fe
yinm <yinm.cminor9@gmail.com>
2017-08-07 21:14:41 +0900

    importが間違えていたので修正

--- chapter03/120/main.go
+++ chapter03/120/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "foo"
+import "./foo"
 
-foo.MAX             // fooパッケージの定数MAXを参照
-foo.internal_const  // コンパイルエラー
+func main() {
+	foo.MAX            // fooパッケージの定数MAXを参照
+	foo.internal_const // コンパイルエラー
 
-foo.FooFunc(5)      // fooパッケージの関数FooFuncを参照
-foo.internalFunc(5) // コンパイルエラー
+	foo.FooFunc(5)      // fooパッケージの関数FooFuncを参照
+	foo.internalFunc(5) // コンパイルエラー
+}
